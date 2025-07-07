24b8ac409a3d63d5b6322f7925cc8ad6e10b9969
sagars <sagarsagar39707@gmail.com>
2025-07-07 16:27:32 +0530

    hi

--- src/main/java/com/shanInfotech/VehicleServiceApp/App.java
+++ src/main/java/com/shanInfotech/VehicleServiceApp/App.java
@@ -9,15 +9,6 @@ public class App {
     	vehicleDao vd=new vehicleDao();
     	System.out.println("Enter the values for vehicle");
     	
-    	vehicles v=new vehicles(sc.nextInt(), sc.next(), sc.next(), sc.next());
-        vd.addvehicles(v);
-        
-        servicerecordDao s=new servicerecordDao();
-        System.out.println("Enter The Value For Service");
-        
-        
-        servicerecord r=new servicerecord(sc.nextInt(),v, sc.next(), sc.next(), sc.nextDouble());
-        
-        s.addservicerecord(r);
+    	vd.getvehicles();
     }
 }
